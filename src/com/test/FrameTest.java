72ea23e1917895decc4394c15ece297a77b837bf
xgh177 <xgh177@sina.com>
2015-09-15 21:45:54 +0800

    第2次提交

---
+++ src/com/test/FrameTest.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class FrameTest extends JFrame{
+	public static void main(String[] args) {
+		 
+		FrameTest wd = new FrameTest();
+		wd.setTitle("ÄãºÃ£¬Ð¡¾²±¦");
+		wd.setSize(400, 200);
+		wd.setBackground(new Color(234,32,22));
+		
+		wd.setVisible(true);
+	}
+
+}
